c9fc2e91b462b3aeb33cb5b6c114b1af9ed4af3d
AntonyMo <lexmqwerty@gmail.com>
2019-05-13 19:16:16 +0300

    Work in progress

--- client.cpp
+++ client.cpp
@@ -9,6 +9,8 @@
 #include <thread>
 #include <csignal>
 #include <sys/stat.h>
+#include <vector>
+#include <string>
 
 #include "client/simpletun.h"
 
@@ -19,6 +21,8 @@ const int BUFF_SIZE = 4* 1024;
 const char *tun_name = "vpn_tun";
 int sock_fd;
 int tap_fd;
+
+bool is_everything_ok = true;
 
 void create_tun(const std::string &vip) {
     std::string syscall = "bash scripts/tun.sh ";
@@ -35,93 +39,39 @@ void delete_tun() {
 }
 
 void term_handler(int i){
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    in_addr in;
-    int res = inet_aton(SERVER_ADDR, &in);
-    if (!res) {
-        exit;
-    }
+    // int s = socket(AF_INET, SOCK_STREAM, 0);
+    // in_addr in;
+    // int res = inet_aton(SERVER_ADDR, &in);
+    // if (!res) {
+    //     exit;
+    // }
 
-    sockaddr_in sockaddr_ = {
-            .sin_family = AF_INET,
-            .sin_port = htons(SERVER_PORT),
-            .sin_addr = in
-    };
-    connect(s, (sockaddr*) &sockaddr_, sizeof(sockaddr_));
-    send(s, (void *)"disconnect", strlen("disconnect"), 0);
+    // sockaddr_in sockaddr_ = {
+    //         .sin_family = AF_INET,
+    //         .sin_port = htons(SERVER_PORT),
+    //         .sin_addr = in
+    // };
+    // connect(s, (sockaddr*) &sockaddr_, sizeof(sockaddr_));
+    // send(s, (void *)"disconnect", strlen("disconnect"), 0);
 
-    close(sock_fd);
-    close(tap_fd);
-    delete_tun();
+    // close(sock_fd);
+    // close(tap_fd);
+    // delete_tun();
 
-    exit(EXIT_SUCCESS);
+    // exit(EXIT_SUCCESS);
+    is_everything_ok = false;
 }
 
 int main() {
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    in_addr in;
-    int res = inet_aton(SERVER_ADDR, &in);
-    if (!res) {
-        return -2;
-    }
-
-    sockaddr_in sockaddr_ = {
-            .sin_family = AF_INET,
-            .sin_port = htons(SERVER_PORT),
-            .sin_addr = in
-    };
 
 
+    // <-- получаешь айпишники серверов
 
-    // Формирование запроса
-    // -----------------------------------------
-
-    std::string request_type;
-    std::string net_name;
-    std::string net_pass;
-
-    std::cin >> request_type;
-
-    // For developers only. This request will be removed from the public version.
-    if(request_type == "shutdown") {
-        connect(s, (sockaddr*) &sockaddr_, sizeof(sockaddr_));
-        send(s, (void *)request_type.c_str(), strlen(request_type.c_str()), 0);
-        return 0;
-    } else if (request_type != "connect" && request_type != "create") {
-        std::cout << "Invalid request type." << std::endl;
-        return 1;
-    }
-
-    std::cin >> net_name >> net_pass;  // пока пусть будет так
-    std::string request = request_type + " " + net_name + " " + net_pass;
-
-    // -----------------------------------------
-
-
-
-    connect(s, (sockaddr*) &sockaddr_, sizeof(sockaddr_));
-    send(s, (void *)request.c_str(), strlen(request.c_str()), 0);
-
-    char buff[BUFF_SIZE];
-    int recv_bytes = recv(s, buff, BUFF_SIZE, 0);
-    std::stringstream dhcp_input(buff);
-
-    std::string vip;
-    dhcp_input >> vip;
-
-    std::cout << buff << std::endl;
-
-    if (vip == "ERR") {
-        std::string error_type;
-        dhcp_input >> error_type;
-        std::cout << "Error occured: " << error_type << std::endl;
-        return 55;
-    }
-    close(s);
-
-    create_tun(vip);
+    std::vector<std::string> ips;
 
     int pid = fork();
+
+    
 
     if (pid == -1) // если не удалось запустить потомка
     {
@@ -146,7 +96,15 @@ int main() {
         //
         std::signal(SIGTERM, term_handler);
 
-        connect_to_server("vpn_tun", SERVER_ADDR, sock_fd, tap_fd);
+        std::vector<std::thread> threads;
+        for (const auto& ip : ips) {
+            threads.emplace_back(threadfunc, std::cref(ip)); // передай номер потока
+        }
+
+        for (auto& thread : threads)
+        {
+            thread.join();
+        }
 
         return 0;
     }
@@ -155,4 +113,51 @@ int main() {
         // завершим процес, т.к. основную свою задачу (запуск демона) мы выполнили
         return 0;
     }
+}
+
+
+void threadfunc(const std::string& addr) {
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    in_addr in;
+    int res = inet_aton(SERVER_ADDR, &in);
+    // if (!res)
+    // {
+    //     return -2;
+    // }
+
+    sockaddr_in sockaddr_ = {
+        .sin_family = AF_INET,
+        .sin_port = htons(SERVER_PORT),
+        .sin_addr = in};
+
+    std::string request = "1";
+
+    // -----------------------------------------
+
+    connect(s, (sockaddr *)&sockaddr_, sizeof(sockaddr_));
+    send(s, (void *)request.c_str(), strlen(request.c_str()), 0);
+
+    char buff[BUFF_SIZE];
+    int recv_bytes = recv(s, buff, BUFF_SIZE, 0);
+    std::stringstream dhcp_input(buff);
+
+    std::string vip;
+    dhcp_input >> vip;
+
+    // std::cout << buff << std::endl;
+
+    // if (vip == "ERR")
+    // {
+    //     std::string error_type;
+    //     dhcp_input >> error_type;
+    //     std::cout << "Error occured: " << error_type << std::endl;
+    //     return 55;
+    // }
+    close(s);
+
+    create_tun(vip);
+                                                                            // работай с интерфейсом vpn_tun(номер потока)
+    connect_to_server("vpn_tun", SERVER_ADDR, sock_fd, tap_fd);
+
+    delete_tun();                                                           // перепиши (наверное)
 }
